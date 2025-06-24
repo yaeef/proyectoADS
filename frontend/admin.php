3af147a5d156bbdd91a90ca5e03cbc7c2be89307
yaef <yaef123@gmail.com>
2025-06-24 14:28:54 -0600

    Cambio de ruta en archivo js

--- frontend/admin.php
+++ frontend/admin.php
@@ -100,7 +100,7 @@
             </div>
         </div>
     </footer>
-    <script src="admin_salones.js"></script>   <!-- Este es el archivo JS donde se encuentra la lógica para mostrar los salones y modales de forma dinámica-->
+    <script src="js/admin_salones.js"></script>   <!-- Este es el archivo JS donde se encuentra la lógica para mostrar los salones y modales de forma dinámica-->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="js/menu.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
